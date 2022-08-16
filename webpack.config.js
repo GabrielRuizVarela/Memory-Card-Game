62146edf4b9ac5d793d720db0de127978269d7f9
Gabriel Ruiz Varela <gabrielruizvarela@gmail.com>
2022-08-16 09:19:36 -0300

    Basic configration

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.jsx',
+  devtool: 'inline-source-map',
+
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        // test: /\.(jsx|js)$/,
+        include: path.resolve(__dirname, 'src/'),
+        exclude: /node_modules/,
+        use:
+        {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', {
+                targets: 'defaults',
+              }],
+              '@babel/preset-react',
+            ],
+          },
+        },
+        resolve: {
+          extensions: ['', '.js', '.jsx'],
+        },
+      },
+      {
+        test: /\.(s(a|c)ss)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(ico|png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+};
